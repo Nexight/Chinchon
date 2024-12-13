42611b3b0a22ff209f34df1b271f17aefdf6a7e5
NicolasQuinterosgit <nicobquinteros@gmail.com>
2024-12-12 23:36:10 -0300

    proyecto

---
+++ Form1.Designer.cs
@@ -0,0 +1,309 @@
+﻿
+namespace Chinchon
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRobarCarta = new System.Windows.Forms.Button();
+            this.btnRepartir = new System.Windows.Forms.Button();
+            this.CartaManoPlayer1 = new System.Windows.Forms.PictureBox();
+            this.CartaManoPlayer2 = new System.Windows.Forms.PictureBox();
+            this.CartaManoPlayer3 = new System.Windows.Forms.PictureBox();
+            this.CartaManoPlayer4 = new System.Windows.Forms.PictureBox();
+            this.CartaManoPlayer5 = new System.Windows.Forms.PictureBox();
+            this.CartaManoPlayer6 = new System.Windows.Forms.PictureBox();
+            this.CartaManoPlayer7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.btnDescartar = new System.Windows.Forms.Button();
+            this.btnTerminarTurno = new System.Windows.Forms.Button();
+            this.lblPuntosRival = new System.Windows.Forms.Label();
+            this.lblPuntosPlayer = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.CartaManoPlayer1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CartaManoPlayer2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CartaManoPlayer3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CartaManoPlayer4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CartaManoPlayer5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CartaManoPlayer6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CartaManoPlayer7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnRobarCarta
+            // 
+            this.btnRobarCarta.Location = new System.Drawing.Point(346, 340);
+            this.btnRobarCarta.Name = "btnRobarCarta";
+            this.btnRobarCarta.Size = new System.Drawing.Size(75, 23);
+            this.btnRobarCarta.TabIndex = 0;
+            this.btnRobarCarta.Text = "Robar Carta";
+            this.btnRobarCarta.UseVisualStyleBackColor = true;
+            this.btnRobarCarta.Click += new System.EventHandler(this.btnRobarCarta_Click);
+            // 
+            // btnRepartir
+            // 
+            this.btnRepartir.Location = new System.Drawing.Point(243, 340);
+            this.btnRepartir.Name = "btnRepartir";
+            this.btnRepartir.Size = new System.Drawing.Size(75, 23);
+            this.btnRepartir.TabIndex = 1;
+            this.btnRepartir.Text = "Repartir";
+            this.btnRepartir.UseVisualStyleBackColor = true;
+            // 
+            // CartaManoPlayer1
+            // 
+            this.CartaManoPlayer1.Location = new System.Drawing.Point(153, 502);
+            this.CartaManoPlayer1.Name = "CartaManoPlayer1";
+            this.CartaManoPlayer1.Size = new System.Drawing.Size(103, 151);
+            this.CartaManoPlayer1.TabIndex = 2;
+            this.CartaManoPlayer1.TabStop = false;
+            // 
+            // CartaManoPlayer2
+            // 
+            this.CartaManoPlayer2.Location = new System.Drawing.Point(262, 502);
+            this.CartaManoPlayer2.Name = "CartaManoPlayer2";
+            this.CartaManoPlayer2.Size = new System.Drawing.Size(103, 151);
+            this.CartaManoPlayer2.TabIndex = 2;
+            this.CartaManoPlayer2.TabStop = false;
+            // 
+            // CartaManoPlayer3
+            // 
+            this.CartaManoPlayer3.Location = new System.Drawing.Point(371, 502);
+            this.CartaManoPlayer3.Name = "CartaManoPlayer3";
+            this.CartaManoPlayer3.Size = new System.Drawing.Size(103, 151);
+            this.CartaManoPlayer3.TabIndex = 2;
+            this.CartaManoPlayer3.TabStop = false;
+            // 
+            // CartaManoPlayer4
+            // 
+            this.CartaManoPlayer4.Location = new System.Drawing.Point(480, 502);
+            this.CartaManoPlayer4.Name = "CartaManoPlayer4";
+            this.CartaManoPlayer4.Size = new System.Drawing.Size(103, 151);
+            this.CartaManoPlayer4.TabIndex = 2;
+            this.CartaManoPlayer4.TabStop = false;
+            // 
+            // CartaManoPlayer5
+            // 
+            this.CartaManoPlayer5.Location = new System.Drawing.Point(589, 502);
+            this.CartaManoPlayer5.Name = "CartaManoPlayer5";
+            this.CartaManoPlayer5.Size = new System.Drawing.Size(103, 151);
+            this.CartaManoPlayer5.TabIndex = 2;
+            this.CartaManoPlayer5.TabStop = false;
+            // 
+            // CartaManoPlayer6
+            // 
+            this.CartaManoPlayer6.Location = new System.Drawing.Point(698, 502);
+            this.CartaManoPlayer6.Name = "CartaManoPlayer6";
+            this.CartaManoPlayer6.Size = new System.Drawing.Size(103, 151);
+            this.CartaManoPlayer6.TabIndex = 2;
+            this.CartaManoPlayer6.TabStop = false;
+            // 
+            // CartaManoPlayer7
+            // 
+            this.CartaManoPlayer7.Location = new System.Drawing.Point(807, 502);
+            this.CartaManoPlayer7.Name = "CartaManoPlayer7";
+            this.CartaManoPlayer7.Size = new System.Drawing.Size(103, 151);
+            this.CartaManoPlayer7.TabIndex = 2;
+            this.CartaManoPlayer7.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(153, 22);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(103, 151);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(262, 22);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(103, 151);
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(371, 22);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(103, 151);
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(480, 22);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(103, 151);
+            this.pictureBox4.TabIndex = 2;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Location = new System.Drawing.Point(589, 22);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(103, 151);
+            this.pictureBox5.TabIndex = 2;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Location = new System.Drawing.Point(698, 22);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(103, 151);
+            this.pictureBox6.TabIndex = 2;
+            this.pictureBox6.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.Location = new System.Drawing.Point(807, 22);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(103, 151);
+            this.pictureBox7.TabIndex = 2;
+            this.pictureBox7.TabStop = false;
+            // 
+            // btnDescartar
+            // 
+            this.btnDescartar.Location = new System.Drawing.Point(480, 340);
+            this.btnDescartar.Name = "btnDescartar";
+            this.btnDescartar.Size = new System.Drawing.Size(75, 23);
+            this.btnDescartar.TabIndex = 1;
+            this.btnDescartar.Text = "Descartar";
+            this.btnDescartar.UseVisualStyleBackColor = true;
+            // 
+            // btnTerminarTurno
+            // 
+            this.btnTerminarTurno.Location = new System.Drawing.Point(589, 334);
+            this.btnTerminarTurno.Name = "btnTerminarTurno";
+            this.btnTerminarTurno.Size = new System.Drawing.Size(75, 35);
+            this.btnTerminarTurno.TabIndex = 1;
+            this.btnTerminarTurno.Text = "Terminar turno";
+            this.btnTerminarTurno.UseVisualStyleBackColor = true;
+            // 
+            // lblPuntosRival
+            // 
+            this.lblPuntosRival.AutoSize = true;
+            this.lblPuntosRival.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.lblPuntosRival.Location = new System.Drawing.Point(150, 214);
+            this.lblPuntosRival.Name = "lblPuntosRival";
+            this.lblPuntosRival.Size = new System.Drawing.Size(25, 13);
+            this.lblPuntosRival.TabIndex = 3;
+            this.lblPuntosRival.Text = "000";
+            // 
+            // lblPuntosPlayer
+            // 
+            this.lblPuntosPlayer.AutoSize = true;
+            this.lblPuntosPlayer.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.lblPuntosPlayer.Location = new System.Drawing.Point(875, 433);
+            this.lblPuntosPlayer.Name = "lblPuntosPlayer";
+            this.lblPuntosPlayer.Size = new System.Drawing.Size(25, 13);
+            this.lblPuntosPlayer.TabIndex = 3;
+            this.lblPuntosPlayer.Text = "000";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Green;
+            this.ClientSize = new System.Drawing.Size(1093, 723);
+            this.Controls.Add(this.lblPuntosPlayer);
+            this.Controls.Add(this.lblPuntosRival);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.CartaManoPlayer7);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.CartaManoPlayer6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.CartaManoPlayer5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.CartaManoPlayer4);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.CartaManoPlayer3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.CartaManoPlayer2);
+            this.Controls.Add(this.CartaManoPlayer1);
+            this.Controls.Add(this.btnTerminarTurno);
+            this.Controls.Add(this.btnDescartar);
+            this.Controls.Add(this.btnRepartir);
+            this.Controls.Add(this.btnRobarCarta);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.CartaManoPlayer1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CartaManoPlayer2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CartaManoPlayer3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CartaManoPlayer4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CartaManoPlayer5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CartaManoPlayer6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CartaManoPlayer7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRobarCarta;
+        private System.Windows.Forms.Button btnRepartir;
+        private System.Windows.Forms.PictureBox CartaManoPlayer1;
+        private System.Windows.Forms.PictureBox CartaManoPlayer2;
+        private System.Windows.Forms.PictureBox CartaManoPlayer3;
+        private System.Windows.Forms.PictureBox CartaManoPlayer4;
+        private System.Windows.Forms.PictureBox CartaManoPlayer5;
+        private System.Windows.Forms.PictureBox CartaManoPlayer6;
+        private System.Windows.Forms.PictureBox CartaManoPlayer7;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.Button btnDescartar;
+        private System.Windows.Forms.Button btnTerminarTurno;
+        private System.Windows.Forms.Label lblPuntosRival;
+        private System.Windows.Forms.Label lblPuntosPlayer;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chinchon
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnRobarCarta_Click(object sender, EventArgs e)
+        {
+            
+            /*Carta carta Mazo.RobarCarta();
+            if (carta != null)
+            {
+                // Añadir la carta a la mano del jugador
+                manoJugador.Add(carta);
+                // Actualizar la interfaz de usuario
+                ActualizarManoJugador();*/
+            }
+            
+
+        }
+    }
+    #region Clases
+    public enum Palo
+    {
+        Oros,
+        Bastos,
+        Espadas,
+        Copas
+    }
+
+    public class Carta
+    {
+        public Palo Palo { get; set; }
+        public int Valor { get; set; }
+        public Image Imagen { get; set; }
+
+        public Carta (Palo palo,int valor, Image imagen)
+        {
+            Palo = palo;
+            Valor = valor;
+            Imagen = imagen;
+        }
+    }
+   public class Mazo
+   {
+        private List<Carta> Cartas;
+
+        public Mazo()
+        {
+            Cartas = new List<Carta>();
+
+        }
+        private void GenerarMazo()
+        {
+            Palo[] palos = (Palo[])Enum.GetValues(typeof(Palo));
+            int[] valores = { 1, 2, 3, 4, 5, 6, 7, 10, 11, 12 };
+
+            foreach(var palo in palos)
+            {
+                foreach(var valor in valores )
+                {
+                    Image imagen = ObtenerImagenDeCarta(palo, valor);
+                    Cartas.Add(new Carta(palo, valor, imagen));
+                }
+            }
+        }
+        private Image ObtenerImagenDeCarta(Palo palo , int valor)
+        {
+            // Falta logica para asignar la imagen correcta
+            return null;
+        }
+
+        public void Mezlcar() // por cada carta en el mazo, toma una carta random y la intercambia con la ultima.
+        {
+            Random random = new Random();
+            int n = Cartas.Count; // da el numero total de cartas que hay 
+            while(n>1) // mientras que el numero total de cartas sea mayor a 1...
+            {
+                n--;
+                int k = random.Next(n + 1); // genera un numero random dentro de la cantidad de cartas que hay
+                Carta value = Cartas[k]; // copia el valor de la carta random elegida y la guarda en value
+                Cartas[k] = Cartas[n]; //la carta random ahora copia los valores de la ultima carta en el mazo (n)
+                Cartas[n] = value; // y la ultima carta del mazo copia los valores originales de la carta random
+
+            }
+
+        }
+        public void Repartir()
+        {
+
+        }
+        public Carta RobarCarta()
+        {
+            if(Cartas.Count>0)
+            {
+                Carta carta = Cartas[0];
+                Cartas.RemoveAt(0);
+                return carta;
+            }
+            return null;
+        }
+   }
+    #endregion
+}
